34739d854ae7e26e0b49a4df62ff2429064d30ba
Melmsie <memyselfandlie@gmail.com>
2022-02-13 12:29:06 -0500

    switch to dev branch

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -5,8 +5,6 @@ const { clientId, guildId, token } = require('../config.json');
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
 ]
 	.map(command => command.toJSON());
 
